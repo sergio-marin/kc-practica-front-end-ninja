5d950b039f05e2300c2653b9115e5338a11fa6e5
Sergio Marin <serjey@gmail.com>
2017-03-03 21:46:31 +0100

    Implementada la visualización de la hora de publicación de los articulos de forma relativa

--- src/js/ready.js
+++ src/js/ready.js
@@ -20,10 +20,9 @@ $(document).ready(function(){
         Animación del Menú al Hacer Scroll
     =================================================*/
     $(window).scroll(function() {    
-        console.log($(window).width());
+        //console.log($(window).width());
         if($(window).width() > 751)
         {
-            console.log("oH YEah");
             var scroll = $(window).scrollTop();
             var element = $(".categories");
             if (scroll >= 500) {
@@ -45,13 +44,13 @@ $(document).ready(function(){
     });
 
     /*================================================
-        Videos Responsive
+        iFrames Responsive
     =================================================*/
     $(function() {
 
-        var $allVideos = $("iframe[src*='vimeo.com'], iframe[src*='youtube.com'], object, embed");
+        var allVideos = $("iframe[src*='vimeo.com'], iframe[src*='youtube.com'], object, embed");
 
-	    $allVideos.each(function() {
+	    allVideos.each(function() {
             $(this).parent().addClass('embed-container');
         });
     });
@@ -78,7 +77,6 @@ $(document).ready(function(){
    $('#search-btn').on('click',function (e) {
         var element =  $("input[name='search']");
         if(element.css('visibility') == 'hidden') {
-            console.log("escondido");
             $('.top-links li').css('visibility', 'hidden');
             element.css('visibility', 'visible');
             element.siblings("a").addClass("active");
@@ -98,6 +96,47 @@ $(document).ready(function(){
             $('.top-links li').css('visibility', 'visible');
         }
     });
+
+    /*================================================
+        Mostrar Fechas de Publicación Relativas
+    =================================================*/
+    var moment = require('moment');
+    var dates = new Array();
+    var i = 0;
+
+    dates.push(moment().subtract(10, 'seconds'));
+    dates.push(moment().subtract(40, 'seconds'));
+    dates.push(moment().subtract(10, 'minutes'));
+    dates.push(moment().subtract(30, 'minutes'));
+    dates.push(moment().subtract(3, 'hours'));
+    dates.push(moment().subtract(9, 'hours'));
+    dates.push(moment().subtract(3, 'days'));
+    dates.push(moment().subtract(8, 'days'));
+    dates.push(moment().subtract(6, 'months'));
+    dates.push(moment().subtract(2, 'years'));
+
+    $('article span.time').each(function(){
+        $(this).attr("date", moment(dates[i]).format());
+        i++;
+    });
+
+    setInterval(function() {
+        $('article span.time').each(function(){
+            var dif = moment().diff(moment($(this).attr("date")));
+            if(dif > 604800000) { // 7 días
+                $(this).text(moment($(this).attr("date")).format("MMM Do YYYY h:mm a"));
+            } else if(dif > 86400000) { // 1 día
+                $(this).text(moment(dif).format("dddd"));
+            } else if(dif > 3600000) { // 1 h
+                $(this).text(moment($(this).attr("date")).fromNow());
+            } else if(dif > 60000) { // 1 min
+                $(this).text(moment(dif).format("m") + " minutes ago" );
+            } else {
+                $(this).text(moment(dif).format("s") + " seconds ago");
+           }
+           
+        });
+    } , 1000);
 
 });
 
